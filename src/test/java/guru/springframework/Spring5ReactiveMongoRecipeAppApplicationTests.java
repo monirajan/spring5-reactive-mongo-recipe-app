927b2468785794691457f24333e6f2a01837c252
monirajan <a.monisha.1995@gmail.com>
2021-03-21 23:17:22 +0530

    Project with Mongodb compliant

---
+++ src/test/java/guru/springframework/Spring5ReactiveMongoRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5ReactiveMongoRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
